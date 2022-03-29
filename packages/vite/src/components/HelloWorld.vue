a43c0e3ff0a7815e8cee2560a6e45472f541d194
StoneRen <stoneren@outlook.com>
2022-03-29 23:37:34 +0800

    [+] vite + vue + tailwind

--- packages/vite/src/components/HelloWorld.vue
+++ packages/vite/src/components/HelloWorld.vue
@@ -9,6 +9,34 @@ const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-  <button type="button" @click="count++">count is: {{ count }}</button>
+  <div
+    class="min-h-screen bg-gray-50 py-6 flex flex-col justify-center relative overflow-hidden sm:py-12"
+  >
+    <h1 class="text-3xl font-bold text-center">{{ msg }}</h1>
+    <hr />
+    <button
+      class="border mx-auto rounded-md w-64 p-5 hover:bg-slate-400 hover:text-slate-100"
+      type="button"
+      @click="count++"
+    >
+      count is: {{ count }}
+    </button>
+  </div>
 </template>
+
+<style scoped>
+hr {
+  box-sizing: content-box;
+  border: 0;
+  line-height: 1.25em;
+  margin: 6px;
+  height: 0.0625rem;
+  background: linear-gradient(
+    to right,
+    transparent,
+    #000 30%,
+    #000 70%,
+    transparent
+  );
+}
+</style>
